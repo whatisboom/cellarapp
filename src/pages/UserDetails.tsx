8538ae6459c43320d5bd5749ed70839ba79ecf9e
Brandon Jernigan <bjernigan@netspend.com>
2018-11-15 11:28:46 -0600

    added user api call on details page

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -1,13 +1,40 @@
 import * as React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { IUserResponse } from 'types';
+import { CellarApiResource } from '../services/api';
 
-interface IUSerDetails {
-  userId: string;
-}
 export class UserDetails extends React.Component<
-  RouteComponentProps<IUSerDetails>
+  RouteComponentProps<{
+    userId: string;
+  }>
 > {
+  public state: any = {
+    user: {},
+    loading: true
+  };
+
+  public resource = new CellarApiResource<
+    {
+      _id: string;
+    },
+    IUserResponse
+  >({
+    path: '/users/:_id'
+  });
+
   public render() {
-    return `user: ${this.props.userId}`;
+    return this.state.loading ? 'loading' : this.state.user.username;
+  }
+
+  public async componentDidMount() {
+    try {
+      const { user } = await this.resource.read({
+        _id: this.props.userId
+      });
+      this.setState({
+        user,
+        loading: false
+      });
+    } catch (e) {}
   }
 }
