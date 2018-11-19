4f695a2fdbe6f8e82427f0008a411a392e1e5b24
Brandon Jernigan <bjernigan@netspend.com>
2018-11-19 12:08:27 -0600

    basic loader support

--- src/components/loaders/loader.tsx
+++ src/components/loaders/loader.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+const styles = require('./loader.css');
 
 export class Loader extends React.Component {
   public render() {
-    return 'LOADING...';
+    return (
+      <div className={styles.container}>
+        <span className={styles.loader} />
+      </div>
+    );
   }
 }
