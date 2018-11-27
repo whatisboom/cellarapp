e0b3d15e3ce0832a94ecb298100737ee4f83ea7c
Brandon Jernigan <brandon@whatisboom.com>
2018-11-26 19:20:32 -0600

    basic jest config and example implemenation on button (#11)
    * basic jest config and example implemenation on button
    
    * added node engine to package.json
    
    * updated circleci config

--- src/components/forms/buttons/submit.tsx
+++ src/components/forms/buttons/submit.tsx
@@ -1,11 +1,11 @@
 import { BaseButton } from './base';
 
 interface ISubmitButton {
-  isPristine: boolean;
-  isInvalid: boolean;
+  isPristine?: boolean;
+  isInvalid?: boolean;
 }
 
 export class SubmitButton extends BaseButton<ISubmitButton> {
-  public text: string = 'submit';
+  public text: string = 'Submit';
   public type: string = 'submit';
 }
