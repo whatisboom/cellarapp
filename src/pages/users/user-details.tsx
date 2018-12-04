97d8ef436a4177f3c8f6236e4ccd1aa3ac1e8d2d
Brandon Jernigan <brandon@whatisboom.com>
2018-12-04 12:48:42 -0600

    user details styles updates

--- src/pages/users/user-details.tsx
+++ src/pages/users/user-details.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps, Link } from '@reach/router';
-import { IQuantity, IUser, IUserResponse } from 'types';
+import { IQuantity, IUser, IUserResponse, IBeer, IBrewery } from 'types';
 import { CellarApiResource } from '../../services/api';
 import { Loader } from '../../components/loaders';
 import Typography from '@material-ui/core/Typography';
@@ -35,18 +35,23 @@ const styles = (theme: Theme) =>
       padding: theme.spacing.unit * 1
     },
     beerName: {
-      fontWeight: 800,
+      fontWeight: theme.typography.fontWeightMedium,
       textDecoration: 'none'
     },
     amount: {
-      color: theme.palette.primary.dark
+      color: theme.palette.getContrastText(theme.palette.background.paper)
     },
     beerList: {
       margin: theme.spacing.unit * 2
     },
     listLink: {
       display: 'block',
-      width: '100%'
+      width: '100%',
+      textDecoration: 'none',
+      color: theme.palette.getContrastText(theme.palette.background.paper)
+    },
+    breweryName: {
+      opacity: 0.6
     }
   });
 
@@ -97,38 +102,7 @@ export class UserDetails extends React.Component<
             </Grid>
           </Grid>
         </Paper>
-        <List
-          className={classes.beerList}
-          component="ul"
-          subheader={<Typography variant="h6">Inventory</Typography>}
-        >
-          {user.owned.map(({ beer, amount }: IQuantity) => {
-            return (
-              <ListItem key={beer._id} disableGutters={true}>
-                <ListItemText
-                  className={classes.beerName}
-                  primary={
-                    <Link
-                      className={classes.listLink}
-                      to={`/beers/${beer.slug}`}
-                    >
-                      {beer.name}
-                    </Link>
-                  }
-                  secondary={
-                    <Link
-                      className={classes.listLink}
-                      to={`/breweries/${beer.brewery.slug}`}
-                    >
-                      {beer.brewery.name}
-                    </Link>
-                  }
-                />
-                <Typography className={classes.amount}>{amount}</Typography>
-              </ListItem>
-            );
-          })}
-        </List>
+        {this.getBeerList('owned', 'Inventory')}
       </React.Fragment>
     );
   }
@@ -143,6 +117,57 @@ export class UserDetails extends React.Component<
         loading: false
       });
     } catch (e) {}
+  }
+
+  public getBeerList(key: 'owned', subheading: string = ''): React.ReactNode {
+    const { user } = this.state;
+    const { classes } = this.props;
+    return (
+      <List
+        className={classes.beerList}
+        component="ul"
+        subheader={<Typography variant="h6">{subheading}</Typography>}
+      >
+        {user[key].map((row: IQuantity) => {
+          return this.getBeerListItem(row);
+        })}
+      </List>
+    );
+  }
+
+  public getBeerListItem({ beer, amount }: IQuantity): React.ReactNode {
+    const { classes } = this.props;
+    return (
+      <ListItem key={beer._id} disableGutters={true}>
+        <ListItemText
+          className={classes.beerName}
+          primary={this.getBeerLink(beer)}
+          secondary={this.getBreweryLink(beer.brewery)}
+        />
+        <Typography className={classes.amount}>{amount}</Typography>
+      </ListItem>
+    );
+  }
+
+  public getBeerLink(beer: IBeer): React.ReactNode {
+    const { classes } = this.props;
+    return (
+      <Link className={classes.listLink} to={`/beers/${beer.slug}`}>
+        {beer.name}
+      </Link>
+    );
+  }
+
+  public getBreweryLink(brewery: IBrewery): React.ReactNode {
+    const { classes } = this.props;
+    return (
+      <Link
+        className={[classes.listLink, classes.breweryName].join(' ')}
+        to={`/breweries/${brewery.slug}`}
+      >
+        {brewery.name}
+      </Link>
+    );
   }
 }
 
