a545ff606643426c939920c35d7f270b3b9c06cb
Brandon Jernigan <brandon@whatisboom.com>
2018-12-09 20:16:04 -0600

    added auth page and links to untappd (#24)

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { SignupForm } from '../components/signup/signup-form';
 
 export class Signup extends React.Component<RouteComponentProps> {
   public render() {
-    return <SignupForm />;
+    return (
+      <a
+        href={`https://untappd.com/oauth/authenticate/?client_id=${
+          process.env.UNTAPPD_CLIENT_ID
+        }&response_type=code&redirect_url=https://app.beercellar.io/auth/oauth/untappd`}
+      >
+        Signup via Untappd
+      </a>
+    );
   }
 }
