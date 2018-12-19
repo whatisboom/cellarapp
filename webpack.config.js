ad16245c4e944a881f16770204a47336166b4876
Brandon Jernigan <brandon@whatisboom.com>
2018-12-19 16:33:07 -0600

    refactor for webpack build

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path'),
+  webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader'
+      },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
