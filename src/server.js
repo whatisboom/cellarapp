f32a56c103a4c3908b2e353aa442445894af16ea
Brandon Jernigan <brandon@whatisboom.com>
2018-12-20 17:09:33 -0600

    implemented pm2 into start process

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || '1234';
+
+app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
