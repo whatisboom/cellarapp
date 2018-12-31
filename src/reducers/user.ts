95f69ca649e776e78ffb62528661085f8863d8e5
Brandon Jernigan <brandon@whatisboom.com>
2018-12-30 22:57:46 -0600

    BEER_REMOVE_FROM_INVENTORY action, added logic in notifications and user

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -1,9 +1,22 @@
-import { UPDATE_LOGGED_IN_USER, LOGOUT } from 'actions';
+import {
+  UPDATE_LOGGED_IN_USER,
+  LOGOUT,
+  BEER_REMOVE_FROM_INVENTORY
+} from 'actions';
+import { IQuantity } from 'types';
 
 export function user(state: any = null, action: any): any {
   switch (action.type) {
     case UPDATE_LOGGED_IN_USER:
       return action.user;
+    case BEER_REMOVE_FROM_INVENTORY:
+      const newState = JSON.parse(JSON.stringify(state));
+      const { owned } = state;
+      const index = owned.findIndex(
+        (item: IQuantity) => item._id === action.owned._id
+      );
+      newState.owned.splice(index, 1);
+      return newState;
     case LOGOUT:
       return {};
     default:
