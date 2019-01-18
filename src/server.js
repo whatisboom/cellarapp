af8dd510b32ee9ed7a12e578c07e2ce75a2199ff
Brandon Jernigan <brandon@whatisboom.com>
2019-01-18 00:27:42 -0600

    fixed server for dev mode

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config');
-const compiler = webpack(webpackConfig);
-
 const app = express();
 const port = process.env.PORT || '1234';
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config');
+  const compiler = webpack(webpackConfig);
 
-app.use(
-  require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: webpackConfig.output.publicPath
-  })
-);
-app.use(require('webpack-hot-middleware')(compiler));
-
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    })
+  );
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 app.use(express.static('dist'));
 
 app.use('*', (req, res) => {
